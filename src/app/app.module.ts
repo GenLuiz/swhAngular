923aece04959189a0aff58b433de07e6ab6bf7c2
Barbosa, Luiz <luiz.barbosa@wipro.com>
2017-09-29 11:51:41 -0300

    skeleton of the project basically done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,31 @@ import { AppComponent } from './app.component';
 import { AppNavComponent } from './app-nav/app-nav.component';
 import { AppHomeComponent } from './app-home/app-home.component';
 import { AppFooterComponent } from './app-footer/app-footer.component';
+import { AppContactComponent } from './app-contact/app-contact.component';
+import { ContactFormComponent } from './app-contact/contact-form/contact-form.component';
+import { ProductComponent } from './product/product.component';
+import { ProductListComponent } from './product/product-list/product-list.component';
+import { ProductDetailComponent } from './product/product-detail/product-detail.component';
+import { AppAboutComponent } from './app-about/app-about.component';
+import { NewsComponent } from './news/news.component';
+import { NewsListComponent } from './news/news-list/news-list.component';
+import { NewsDetailComponent } from './news/news-detail/news-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AppNavComponent,
     AppHomeComponent,
-    AppFooterComponent
+    AppFooterComponent,
+    AppContactComponent,
+    ContactFormComponent,
+    ProductComponent,
+    ProductListComponent,
+    ProductDetailComponent,
+    AppAboutComponent,
+    NewsComponent,
+    NewsListComponent,
+    NewsDetailComponent,
   ],
   imports: [
     BrowserModule
